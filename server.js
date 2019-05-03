b1124de1ad0c0a272522f0b216f33f5bee976ce3
James DeVore <jdevore4592@gmail.com>
2019-05-03 12:34:01 -0400

    comimt before deploy

--- server.js
+++ server.js
@@ -15,6 +15,23 @@ app.use(
   })
 );
 
+//for deployment
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
 //routes
 
 app.use('/api/cards', cards)
